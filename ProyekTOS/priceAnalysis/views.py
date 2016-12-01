bc838e6ca67fbe705c542329f83810b25413cc32
Satria Damai <m26415172@john.petra.ac.id>
2016-12-01 09:13:46 +0700

    update

--- ProyekTOS/priceAnalysis/views.py
+++ ProyekTOS/priceAnalysis/views.py
@@ -9,25 +9,30 @@ from urllib2 import Request, urlopen, URLError, HTTPError
 import subprocess
 
 def index(request):
+#ini handle request yang masuk
     if request.method == 'POST':
         print 'Masuk Post'
         jenis = int(request.POST.get('cabe'))
         m = int(request.POST.get('m'))
         y = int(request.POST.get('y'))
+#else ini berarti default awal (input bukan merupakan pilihan user)
     else:
         jenis = 1
         m = 11
         y = 2016
+#last dan lyear untuk perhitungan SMA dari bulan bulan sebelumnya
     last = m - 1
     lyear = y
     if last <= 0:
         last = 12
         lyear = y - 1
+#download source sesuai request jenis cabe, bulan dan tahun
     sourceUrl = "http://infopangan.jakarta.go.id/api/price/series_by_location?public=1&type=city&lid=2&m="
     finalUrl = sourceUrl + str(m) + "&y=" + str(y)
     lastUrl = sourceUrl + str(last) + "&y=" + str(lyear)
     req = Request(finalUrl)
     req2 = Request(lastUrl)
+#persiapan file untuk menampung hasil source download
     file = open('./priceAnalysis/data/source.txt','w')
     file2 = open('./priceAnalysis/data/last.txt','w')
     try:
@@ -47,10 +52,10 @@ def index(request):
         print 'everthing is fine'
         file.close()
         file2.close()
-
+#menjalankan shell script untuk grep dan sed (pengolahan data)
     subprocess.call('./priceAnalysis/processing.sh ./priceAnalysis/data/source',shell=True)
     subprocess.call('./priceAnalysis/processing.sh ./priceAnalysis/data/last', shell=True)
-
+#mempersiapkan data untuk grafik dalam list
     x = [x for x in range(1,32)]
     if jenis == 1:
         f = open('./priceAnalysis/data/source1.txt','r')
@@ -70,7 +75,7 @@ def index(request):
         l.close()
         y0 = [int(i) for i in data if i != data[-1]]
         late = [int(i) for i in last[-6:-1]]
-#    SMA
+#   perhitungan SMA
     late.extend(y0)
     isi = len(late)
     sma = 0
@@ -85,7 +90,7 @@ def index(request):
     y0.append(sma)
     print y0, len(y0)
     print y1, len(y1)
-
+#mempersiapkan grafik menggunakan bokeh
     plot = figure(tools="pan,wheel_zoom,reset",
     plot_width=1000, plot_height=600,
     title="Kurva Cabe",
